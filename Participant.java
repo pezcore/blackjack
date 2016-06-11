7099032f56ba32c87a3bf9875343b04407f2f2e8
Tom Boyd <tboyd@scitec.com>
2016-06-11 15:21:47 -0400

    fixed hand printing to not have trailing commas

--- Participant.java
+++ Participant.java
@@ -4,12 +4,15 @@ public class Participant{
     int wins, losses;
     ArrayList<Byte> hand = new ArrayList<Byte>(21); // needs only hold 21 cards
     public void printHand(){
+        byte counter = 0;
         System.out.print("[");
         for (byte card : hand){
             if (card != (byte) 11)
-                System.out.printf("%d,",card);
+                System.out.printf("%d",card);
             else
-                System.out.printf("A,");
+                System.out.print("A");
+            if (counter++ < hand.size()-1)
+                System.out.print(",");
         }
         System.out.printf("]");
     }

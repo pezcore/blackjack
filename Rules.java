00952908558bd314e6daa8315a1bce03e1a7b55f
Tom Boyd <tboyd@scitec.com>
2016-06-25 12:22:59 -0400

    Added Rules Struct Class

---
+++ Rules.java
@@ -0,0 +1,6 @@
+public class Rules{
+    public boolean hitSoft17s = true;
+    public boolean doubleAfterSplitAllowed = true;
+    public boolean doubleAllowed = true;
+    public boolean surrenderAllowed = true;
+}
